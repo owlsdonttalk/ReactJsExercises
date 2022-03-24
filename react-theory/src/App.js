60847b13ac9b6b0ed1c16cfb0090d07b787991d9
i.petrov <ivan@miropolskiy.com>
2022-03-24 21:09:37 +0300

    lesson3

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -3,22 +3,81 @@ import React, {Component} from "react";
 import Car from "./Car/Car";
 
 class App extends Component {
+
+	state = {
+		cars: [
+			{name: 'Ford', year: '2018'},
+			{name: 'Audi', year: '2016'},
+			{name: 'Mazda', year: '2014'}
+		],
+		pageTitle: 'React Components',
+		showCars: false,
+	}
+
+	changeTitleHandler = (pageTitle) => {
+		this.setState({pageTitle})
+	}
+
+	handleInput = (event) => {
+		this.setState({
+			pageTitle: event.target.value
+		})
+	}
+
+	toggleCarsHandler = () =>
+	{
+		this.setState({
+			showCars: !this.state.showCars
+		})
+	}
+
+	deleteHandler(index) {
+		const cars = this.state.cars.concat()
+		cars.splice(index, 1);
+
+		this.setState({cars})
+	}
+
+	onChangeName(name, index) {
+		console.log(name, index)
+		const car = this.state.cars[index]
+		car.name = name
+
+		const cars = [...this.state.cars]
+		cars[index] = car
+		this.setState({cars})
+	}
+
 	render()
 	{
+		let cars = null
+
+		if(this.state.showCars) {
+			cars =  this.state.cars.map((car, index) => {
+				return (
+					<Car
+						key={index}
+						name={car.name}
+						year={car.year}
+						onDelete={this.deleteHandler.bind(this, index)}
+						onChangeName={event => this.onChangeName(event.target.value, index)}
+					/>
+				)
+			})
+		}
+
 		const divStyle = {
 			textAlign: 'center',
 		}
 
 		return (
+
 			<div style={divStyle}>
-				<h1>Hello,world!</h1>
-				<Car name={'Ford'} year={'2018'}>
-					<p style={{color: 'blue'}}>BLUE</p>
-				</Car>
-				<Car name={'Audi'} year={'2016'}>
-					<p style={{color: 'red'}}>COLOR</p>
-				</Car>
-				<Car name={'Mazda'} year={'2015'}/>
+				<h1>{this.state.pageTitle}</h1>
+
+				<button onClick={this.toggleCarsHandler}>Toggle cars</button>
+
+				{ cars }
 			</div>
 		);
 	}
--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -1,19 +1,16 @@
 import React from 'react'
 
-//function Car() {
-//	return(
-//
-//	)
-//}
-
-const Car = (props) => {
-	return(
-		<div>
+export default props => (
+		<div style={{
+			border: '1px solid #ccc',
+			marginBottom : '10px',
+			display: 'block',
+			padding: '10px'
+		}}>
 			<h3>Car name: {props.name}</h3>
 			<p> Year: <strong>{props.year}</strong></p>
-			{ props.children }
+			<input type="text" onChange={props.onChangeName} value={props.name}/>
+			<button onClick={props.onDelete}>Delete</button>
 		</div>
-	)
-}
+)
 
-export default Car
